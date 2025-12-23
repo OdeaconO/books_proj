1c457b1ee5ba4eeff0dfef072dfcf64a1b3aafb1
Dhruv Rai <iideaconii@Dhruvs-MacBook-Air.local>
2025-12-23 17:34:05 +0530

    fully implemented protected frontend routes, guest routes, unknown route handling and auto logout

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import{
   Route,
   Navigate,
 } from "react-router-dom";
+import { useEffect } from "react";
+import { isTokenExpired } from "./utils/isTokenExpired";
 import ProtectedRoute from "./ProtectedRoute";
+import GuestRoute from "./GuestRoute";
 import Books from "./pages/Books";
 import Add from "./pages/Add";
 import Update from "./pages/Update";
@@ -13,17 +16,28 @@ import Register from "./pages/Register";
 import "./style.css";
 
 function App() {
+
+  useEffect(() => {
+  const token = localStorage.getItem("token");
+
+  if (token && isTokenExpired(token)) {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  }
+}, []);
+
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login"/>}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<GuestRoute><Login /></GuestRoute>} />
+        <Route path="/register" element={<GuestRoute><Register /></GuestRoute>} />
 
         <Route path="/books" element={<ProtectedRoute><Books /></ProtectedRoute>} />
         <Route path="/add" element={<ProtectedRoute><Add/></ProtectedRoute>}/>
         <Route path="/update/:id" element={<ProtectedRoute><Update/></ProtectedRoute>}/>
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
       </BrowserRouter>
     </div>
